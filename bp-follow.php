67dfb0623445074b22d77c569b5bb6c039149a48
r-a-y <r-a-y@gmx.com>
2013-03-03 12:34:04 -0800

    Fix warnings.

--- bp-follow.php
+++ bp-follow.php
@@ -32,8 +32,13 @@ function bp_follow_setup_globals() {
 	if ( !defined( 'BP_FOLLOWING_SLUG' ) )
 		define( 'BP_FOLLOWING_SLUG', 'following' );
 
+	// Initialize follow objects
+	$bp->follow            = new stdClass;
+	$bp->follow->followers = new stdClass;
+	$bp->follow->following = new stdClass;
+
 	// For internal identification
-	$bp->follow->id              = 'follow';
+	$bp->follow->id = 'follow';
 
 	$bp->follow->table_name      = $bp->table_prefix . 'bp_follow';
 	$bp->follow->followers->slug = BP_FOLLOWERS_SLUG;
