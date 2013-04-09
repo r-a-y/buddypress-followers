fa9fe12e9b3ca983930e2ac9f0710e29ee68eacb
r-a-y <r-a-y@gmx.com>
2013-04-08 19:39:45 -0700

    Locally cache total follow counts for logged-in user and displayed user.

--- _inc/bp-follow-functions.php
+++ _inc/bp-follow-functions.php
@@ -159,7 +159,33 @@ function bp_follow_total_follow_counts( $args = '' ) {
 
 	$r = wp_parse_args( $args, $defaults );
 
-	return apply_filters( 'bp_follow_total_follow_counts', BP_Follow::get_counts( $r['user_id'] ) );
+	$count = false;
+
+	/* try to get locally-cached values first */
+
+	// logged-in user
+	if ( $r['user_id'] == bp_loggedin_user_id() && is_user_logged_in() ) {
+		global $bp;
+
+		if ( ! empty( $bp->loggedin_user->total_follow_counts ) ) {
+			$count = $bp->loggedin_user->total_follow_counts;
+		}
+
+	// displayed user
+	} elseif ( $r['user_id'] == bp_displayed_user_id() && bp_is_user() ) {
+		global $bp;
+		
+		if ( ! empty( $bp->displayed_user->total_follow_counts ) ) {
+			$count = $bp->displayed_user->total_follow_counts;
+		}
+	}
+
+	// no cached value, so query for it
+	if ( $count === false ) {
+		$count = BP_Follow::get_counts( $r['user_id'] );
+	}
+
+	return apply_filters( 'bp_follow_total_follow_counts', $count, $r['user_id'] );
 }
 
 /**
