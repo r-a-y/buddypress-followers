b1787a09e46d57169927e43d3ef285ecc9cb1f45
r-a-y <r-a-y@gmx.com>
2013-04-08 18:08:41 -0700

    Cleanup BP_Follow_Following_Widget class.
    * Add ability to change the widget title
    * Add CSS class to widget for 1.7 compatibility
    * Do not use extract()

--- _inc/bp-follow-widgets.php
+++ _inc/bp-follow-widgets.php
@@ -15,59 +15,96 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * @subpackage Widgets
  */
 class BP_Follow_Following_Widget extends WP_Widget {
-	function bp_follow_following_widget() {
-		parent::WP_Widget( false, $name = __( "Users I'm Following", 'bp-follow' ) );
+	/**
+	 * Constructor.
+	 */
+	function __construct() {
+		// Set up optional widget args
+		$widget_ops = array(
+			'classname'   => 'widget buddypress widget-logged-in-following',
+			'description' => __( "Show a list of member avatars that the logged-in user is following.", 'bp-follow' )
+		);
+
+		// Set up the widget
+		parent::WP_Widget(
+			false,
+			__( "(BP Follow) Users I'm Following", 'bp-follow' ),
+			$widget_ops
+		);
 	}
 
+	/**
+	 * Displays the widget.
+	 */
 	function widget( $args, $instance ) {
+		// do not do anything if user isn't logged in
+		if ( ! is_user_logged_in() )
+			return;
 
-		extract( $args );
+		if ( empty( $instance['max_users'] ) ) {
+			$instance['max_users'] = 16;
+		}
 
-		if ( empty( $instance['max_users'] ) )
-			$instance['max_users'] = 25;
-
-		if ( !$following = bp_get_following_ids( array( 'user_id' => bp_loggedin_user_id() ) ) )
+		// logged-in user isn't following anyone, so stop!
+		if ( ! $following = bp_get_following_ids( array( 'user_id' => bp_loggedin_user_id() ) ) ) {
 			return false;
+		}
 
+		// show the users the logged-in user is following
 		if ( bp_has_members( 'include=' . $following . '&max=' . $instance['max_users'] ) ) {
-
 			do_action( 'bp_before_following_widget' );
 
-			echo $before_widget;
-			echo $before_title
-			   . __( 'Following', 'bp-follow' )
-			   . $after_title; ?>
+			echo $args['before_widget'];
+			echo $args['before_title']
+			   . $instance['title']
+			   . $args['after_title'];
+	?>
 
 			<div class="avatar-block">
 				<?php while ( bp_members() ) : bp_the_member(); ?>
 					<div class="item-avatar">
-						<a title="<?php bp_member_name() ?>" href="<?php bp_member_permalink() ?>"><?php bp_member_avatar() ?></a>
+						<a href="<?php bp_member_permalink() ?>" title="<?php bp_member_name() ?>"><?php bp_member_avatar() ?></a>
 					</div>
 				<?php endwhile; ?>
 			</div>
 
-			<?php echo $after_widget; ?>
+			<?php echo $args['after_widget']; ?>
 
 			<?php do_action( 'bp_after_following_widget' ); ?>
 
-		<?php } ?>
-
 	<?php
+		}
 	}
 
+	/**
+	 * Callback to save widget settings.
+	 */
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
-		$instance['max_users'] = strip_tags( $new_instance['max_users'] );
+		$instance['title']     = strip_tags( $new_instance['title'] );
+		$instance['max_users'] = (int) $new_instance['max_users'];
 
 		return $instance;
 	}
 
+	/**
+	 * Widget settings form.
+	 */
 	function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'max_users' => 25 ) );
-		$max_users = strip_tags( $instance['max_users'] );
-		?>
+		$defaults = array(
+			'title'     => __( "Users I'm Following", 'bp-follow' ),
+			'max_users' => 16
+		);
 
-		<p><label for="bp-follow-widget-users-max"><?php _e('Max users to show:', 'bp-follow'); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'max_users' ); ?>" name="<?php echo $this->get_field_name( 'max_users' ); ?>" type="text" value="<?php echo attribute_escape( $max_users ); ?>" style="width: 30%" /></label></p>
+		$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" /></p>
+
+		<p><label for="bp-follow-widget-users-max"><?php _e('Max members to show:', 'bp-follow'); ?> <input class="widefat" id="<?php echo $this->get_field_id( 'max_users' ); ?>" name="<?php echo $this->get_field_name( 'max_users' ); ?>" type="text" value="<?php echo esc_attr( (int) $instance['max_users'] ); ?>" style="width: 30%" /></label></p>
+		<p><small><?php _e( 'Note: This widget is only displayed if a member is logged in and if the logged-in user is following some users.', 'bp-follow' ); ?></small></p>
+
 	<?php
 	}
 }
