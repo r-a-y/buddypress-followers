de5d1c534d012da8da916507acfbe0413f642501
r-a-y <r-a-y@gmx.com>
2014-03-03 15:17:07 -0800

    Add support to mark notifications as unread.
    This commit changes how notifications are cleared.
    
    Previously, follow notifications were redirected to the logged-in
    user's followers page and deleted immediately.  Now, follow
    notifications are redirected to the follower's profile and
    marked as read.
    
    For the bulk follow notification ("X users are now
    following you"), the logged-in user is now redirected to the
    notifications unread page and not marked as read or deleted.  This
    allows the logged-in user to view their follower notifications in the
    date that they were followed.
    
    Fixes #24.

--- _inc/bp-follow-notifications.php
+++ _inc/bp-follow-notifications.php
@@ -23,14 +23,22 @@ function bp_follow_format_notifications( $action, $item_id, $secondary_item_id, 
 
 	switch ( $action ) {
 		case 'new_follow':
-			$link = bp_loggedin_user_domain() . $bp->follow->followers->slug . '/?new';
+			$link = $text = false;
 
 			if ( 1 == $total_items ) {
-				$text = __( '1 more user is now following you', 'bp-follow' );
-			}
-			else {
+				$text = sprintf( __( '%s is now following you', 'bp-follow' ), bp_core_get_user_displayname( $item_id ) );
+				$link = bp_core_get_user_domain( $item_id  ) . '?bpf_read';
+
+			} else {
 				$text = sprintf( __( '%d more users are now following you', 'bp-follow' ), $total_items );
+
+				if ( bp_is_active( 'notifications' ) ) {
+					$link = bp_get_notifications_permalink();				
+				} else {
+					$link = bp_loggedin_user_domain() . $bp->follow->followers->slug . '/?new';
+				}
 			}
+
 		break;
 
 		default :
@@ -43,7 +51,7 @@ function bp_follow_format_notifications( $action, $item_id, $secondary_item_id, 
 		return false;
 
 	if ( 'string' == $format ) {
-		return apply_filters( 'bp_follow_new_followers_notification', '<a href="' . $link . '" title="' . __( 'Your list of followers', 'bp-follow' ) . '">' . $text . '</a>', $total_items, $link, $text, $item_id, $secondary_item_id );
+		return apply_filters( 'bp_follow_new_followers_notification', '<a href="' . $link . '">' . $text . '</a>', $total_items, $link, $text, $item_id, $secondary_item_id );
 	}
 	else {
 		$array = array(
@@ -56,7 +64,7 @@ function bp_follow_format_notifications( $action, $item_id, $secondary_item_id, 
 }
 
 /**
- * Removes notification data for a user.
+ * Removes notifications made by a user.
  *
  * @since 1.2.1
  */
@@ -76,20 +84,90 @@ function bp_follow_remove_notification_data( $user_id = 0 ) {
 add_action( 'bp_follow_remove_data', 'bp_follow_remove_notification_data' );
 
 /**
- * Delete notifications when a logged-in user visits their followers page.
+ * Mark notification as read when a logged-in user visits their follower's profile.
  *
- * Only do this for users on < BuddyPress 1.9.
+ * This is a new feature in BuddyPress 1.9.
  *
  * @since 1.2.1
  */
-function bp_follow_deprecated_delete_notifications() {
-	if ( ! bp_is_active( 'notifications' ) && ! function_exists( 'BP_Core_Login_Widget' ) && isset( $_GET['new'] ) ) {
+function bp_follow_notifications_mark_follower_profile_as_read() {
+	if ( ! isset( $_GET['bpf_read'] ) ) {
+		return;
+	}
+
+	// mark notification as read
+	if ( bp_is_active( 'notifications' ) ) {
+		bp_notifications_mark_notifications_by_item_id( bp_loggedin_user_id(), bp_displayed_user_id(), buddypress()->follow->id, 'new_follow' );
+
+	// check if we're not on BP 1.9
+	// if so, delete notification since marked functionality doesn't exist
+	} elseif ( ! class_exists( 'BP_Core_Login_Widget' ) ) {
+		global $bp;
+
+		bp_core_delete_notifications_by_item_id( bp_loggedin_user_id(), bp_displayed_user_id(), $bp->follow->id, 'new_follow' );
+	}
+}
+add_action( 'bp_members_screen_display_profile', 'bp_follow_notifications_mark_follower_profile_as_read' );
+
+/**
+ * Delete notifications when a logged-in user visits their followers page.
+ *
+ * Since 1.2.1, when the "X users are now following you" notification appears,
+ * users will be redirected to the new notifications unread page instead of
+ * the logged-in user's followers page.  This is so users can see who followed
+ * them and in the date order in which they were followed.
+ *
+ * For backwards-compatibility, we still keep the old method of redirecting to
+ * the logged-in user's followers page so notifications can be deleted for
+ * older versions of BuddyPress.
+ *
+ * Will probably remove this in a future release.
+ *
+ * @since 1.2.1
+ */
+function bp_follow_notifications_delete_on_followers_page() {
+	if ( ! isset( $_GET['new'] ) ) {
+		return;
+	}
+
+	if ( ! is_user_logged_in() ) {
+		return;
+	}
+
+	if ( bp_is_active( 'notifications' ) ) {
+		bp_notifications_delete_notifications_by_type( bp_loggedin_user_id(), $bp->follow->id, 'new_follow' );
+	} else {
 		global $bp;
 
 		bp_core_delete_notifications_by_type( bp_loggedin_user_id(), $bp->follow->id, 'new_follow' );
 	}
 }
-add_action( 'bp_follow_screen_followers', 'bp_follow_deprecated_delete_notifications' );
+add_action( 'bp_follow_screen_followers', 'bp_follow_notifications_delete_on_followers_page' );
+
+/**
+ * When we're on the notification's 'read' page, remove 'bpf_read' query arg.
+ *
+ * Since we are already on the 'read' page, notifications on this page are
+ * already marked as read.  So, we no longer need to add our special
+ * 'bpf_read' query argument to each notification to determine whether we
+ * need to clear it.
+ *
+ * @since 1.2.1
+ */
+function bp_follow_notifications_remove_queryarg_from_userlink( $retval ) {
+	if ( bp_is_current_action( 'read' ) ) {
+		// if notifications loop has finished rendering, stop now!
+		// this is so follow notifications in the adminbar are unaffected
+		if ( did_action( 'bp_after_member_body' ) ) {
+			return $retval;
+		}
+
+		$retval = str_replace( '?bpf_read', '', $retval );
+	}
+
+	return $retval;
+}
+add_filter( 'bp_follow_new_followers_notification', 'bp_follow_notifications_remove_queryarg_from_userlink' );
 
 /** SETTINGS ************************************************************/
 
@@ -163,7 +241,7 @@ function bp_follow_new_follow_email_notification( $args = '' ) {
 	bp_update_user_meta( $r['follower_id'], 'bp_follow_has_notified', $has_notified );
 
 	$follower_name = bp_core_get_user_displayname( $r['follower_id'] );
-	$follower_link = bp_core_get_user_domain( $r['follower_id'] );
+	$follower_link = bp_core_get_user_domain( $r['follower_id'] ) . '?bpf_read';
 
 	$leader_ud = bp_core_get_core_userdata( $r['leader_id'] );
 
