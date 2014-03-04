56afeaa41a964eda9cd9702c235b65ad06332716
r-a-y <r-a-y@gmx.com>
2014-03-03 20:03:25 -0800

    Tweaks to action functions.

--- _inc/bp-follow-actions.php
+++ _inc/bp-follow-actions.php
@@ -10,80 +10,80 @@
 if ( !defined( 'ABSPATH' ) ) exit;
 
 /**
- * Catches clicks on a "Follow User" button and tries to make that happen.
+ * Catches clicks on a "Follow" button and tries to make that happen.
  *
- * @global $bp The global BuddyPress settings variable created in bp_core_setup_globals()
  * @uses check_admin_referer() Checks to make sure the WP security nonce matches.
  * @uses bp_follow_is_following() Checks to see if a user is following another user already.
  * @uses bp_follow_start_following() Starts a user following another user.
  * @uses bp_core_add_message() Adds an error/success message to be displayed after redirect.
  * @uses bp_core_redirect() Safe redirects the user to a particular URL.
- * @return bool false
  */
 function bp_follow_action_start() {
 	global $bp;
 
-	if ( !bp_is_current_component( $bp->follow->followers->slug ) || !bp_is_current_action( 'start' ) )
-		return false;
+	if ( ! bp_is_current_component( $bp->follow->followers->slug ) || ! bp_is_current_action( 'start' ) ) {
+		return;
+	}
 
-	if ( bp_displayed_user_id() == bp_loggedin_user_id() )
-		return false;
+	if ( bp_displayed_user_id() == bp_loggedin_user_id() ) {
+		return;
+	}
 
 	check_admin_referer( 'start_following' );
 
-	if ( bp_follow_is_following( array( 'leader_id' => bp_displayed_user_id(), 'follower_id' => bp_loggedin_user_id() ) ) )
+	if ( bp_follow_is_following( array( 'leader_id' => bp_displayed_user_id(), 'follower_id' => bp_loggedin_user_id() ) ) ) {
 		bp_core_add_message( sprintf( __( 'You are already following %s.', 'bp-follow' ), bp_get_displayed_user_fullname() ), 'error' );
-	else {
-		if ( !bp_follow_start_following( array( 'leader_id' => bp_displayed_user_id(), 'follower_id' => bp_loggedin_user_id() ) ) )
+
+	} else {
+		if ( ! bp_follow_start_following( array( 'leader_id' => bp_displayed_user_id(), 'follower_id' => bp_loggedin_user_id() ) ) ) {
 			bp_core_add_message( sprintf( __( 'There was a problem when trying to follow %s, please try again.', 'bp-follow' ), bp_get_displayed_user_fullname() ), 'error' );
-		else
+		} else {
 			bp_core_add_message( sprintf( __( 'You are now following %s.', 'bp-follow' ), bp_get_displayed_user_fullname() ) );
+		}
 	}
 
 	// it's possible that wp_get_referer() returns false, so let's fallback to the displayed user's page
 	$redirect = wp_get_referer() ? wp_get_referer() : bp_displayed_user_domain();
 	bp_core_redirect( $redirect );
-
-	return false;
 }
 add_action( 'bp_actions', 'bp_follow_action_start' );
 
 /**
- * Catches clicks on a "Stop Following User" button and tries to make that happen.
+ * Catches clicks on a "Unfollow" button and tries to make that happen.
  *
- * @global $bp The global BuddyPress settings variable created in bp_core_setup_globals()
  * @uses check_admin_referer() Checks to make sure the WP security nonce matches.
  * @uses bp_follow_is_following() Checks to see if a user is following another user already.
  * @uses bp_follow_stop_following() Stops a user following another user.
  * @uses bp_core_add_message() Adds an error/success message to be displayed after redirect.
  * @uses bp_core_redirect() Safe redirects the user to a particular URL.
- * @return bool false
  */
 function bp_follow_action_stop() {
 	global $bp;
 
-	if ( !bp_is_current_component( $bp->follow->followers->slug ) || !bp_is_current_action( 'stop' ) )
-		return false;
+	if ( ! bp_is_current_component( $bp->follow->followers->slug ) || ! bp_is_current_action( 'stop' ) ) {
+		return;
+	}
 
-	if ( bp_displayed_user_id() == bp_loggedin_user_id() )
-		return false;
+	if ( bp_displayed_user_id() == bp_loggedin_user_id() ) {
+		return;
+	}
 
 	check_admin_referer( 'stop_following' );
 
-	if ( !bp_follow_is_following( array( 'leader_id' => bp_displayed_user_id(), 'follower_id' => bp_loggedin_user_id() ) ) )
+	if ( ! bp_follow_is_following( array( 'leader_id' => bp_displayed_user_id(), 'follower_id' => bp_loggedin_user_id() ) ) ) {
 		bp_core_add_message( sprintf( __( 'You are not following %s.', 'bp-follow' ), bp_get_displayed_user_fullname() ), 'error' );
-	else {
-		if ( !bp_follow_stop_following( array( 'leader_id' => bp_displayed_user_id(), 'follower_id' => bp_loggedin_user_id() ) ) )
+
+	} else {
+		if ( ! bp_follow_stop_following( array( 'leader_id' => bp_displayed_user_id(), 'follower_id' => bp_loggedin_user_id() ) ) ) {
 			bp_core_add_message( sprintf( __( 'There was a problem when trying to stop following %s, please try again.', 'bp-follow' ), bp_get_displayed_user_fullname() ), 'error' );
-		else
+		} else {
 			bp_core_add_message( sprintf( __( 'You are no longer following %s.', 'bp-follow' ), bp_get_displayed_user_fullname() ) );
+		}
 	}
 
 	// it's possible that wp_get_referer() returns false, so let's fallback to the displayed user's page
 	$redirect = wp_get_referer() ? wp_get_referer() : bp_displayed_user_domain();
 	bp_core_redirect( $redirect );
-
-	return false;
 }
 add_action( 'bp_actions', 'bp_follow_action_stop' );
 
