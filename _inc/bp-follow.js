r-a-y/buddypress-followers

commit 8a414c1d878e1d51e8bd1c9d2c5b3d862b1b8f93

Fix incremental count when a user clicks on a follow button on their own following pages.
Previously, if a user is on their own following / followers pages and
they unfollowed or followed a user, the "Following" count would not
increase or decrease.

This commit fixes this.

See #14.

_inc/bp-follow.js
@@ -13,7 +13,7 @@ jq( function() {
 	});
 
 	memberLoop.on("click", ".follow-button a", function() {
-		bp_follow_button_action( jq(this), 'member-directory' );
+		bp_follow_button_action( jq(this), 'member-loop' );
 		return false;
 	});
 
@@ -57,8 +57,16 @@ jq( function() {
 				var count_wrapper = false;
 				if ( context == 'profile' ) {
 					count_wrapper = jq("#user-members-followers span");
-				} else if ( context == 'member-directory' ) {
-					count_wrapper = jq("#members-following span");
+
+				} else if ( context == 'member-loop' ) {
+					// a user is on their own profile
+					if ( ! jq.trim( profileHeader.text() ) ) {
+						count_wrapper = jq("#user-members-following span");
+
+					// this means we're on the member directory
+					} else {
+						count_wrapper = jq("#members-following span");
+					}
 				}
 
 				if ( count_wrapper.length ) {

commit 93dcca4522198b6e4bff2ccdfb7a73d42d369a80

Fix incremental count when unfollowing user on members directory.
Commit 8a414c1 broke the incremental count on the members directory. This
commit fixes this.

See #14.

_inc/bp-follow.js
@@ -59,13 +59,13 @@ jq( function() {
 					count_wrapper = jq("#user-members-followers span");
 
 				} else if ( context == 'member-loop' ) {
-					// a user is on their own profile
-					if ( ! jq.trim( profileHeader.text() ) ) {
-						count_wrapper = jq("#user-members-following span");
-
 					// this means we're on the member directory
-					} else {
+					if ( jq(".dir-search").length ) {
 						count_wrapper = jq("#members-following span");
+
+					// a user is on their own profile
+					} else if ( ! jq.trim( profileHeader.text() ) ) {
+						count_wrapper = jq("#user-members-following span");
 					}
 				}
 