564708cbb82f5f62f16ca179827e80dd0ec03f34
Christian Wach <needle@haystack.co.uk>
2014-06-11 19:56:53 +0100

    return template path

--- _inc/bp-follow-blogs.php
+++ _inc/bp-follow-blogs.php
@@ -236,9 +236,9 @@ class BP_Follow_Blogs {
 	 *
 	 * @todo Maybe add this in BP Core?
 	 */
-	function set_activity_scope_on_activity_directory() {
+	function set_activity_scope_on_activity_directory( $template ) {
 		if ( empty( $_GET['scope'] ) ) {
-			return;
+			return $template;
 		}
 
 		$scope = wp_filter_kses( $_GET['scope'] );
@@ -251,6 +251,8 @@ class BP_Follow_Blogs {
 
 		//reset the dropdown menu to 'Everything'
 		@setcookie( 'bp-activity-filter', '-1',   0, '/' );
+		
+		return $template;
 	}
 
 	/**
