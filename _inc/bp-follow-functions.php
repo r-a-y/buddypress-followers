ca2e84d41bdc6b8476965fc56035a9286ab1fc7f
r-a-y <r-a-y@gmx.com>
2014-08-14 10:22:52 -0700

    Move some functions from 'bp-follow-templatetags.php' to 'bp-follow-functions.php'.
    Since there are no more functions in bp-follow-templatetags.php, we're
    also going to remove this file.
    
    See #46.

--- _inc/bp-follow-functions.php
+++ _inc/bp-follow-functions.php
@@ -185,6 +185,76 @@ function bp_follow_get_following( $args = '' ) {
 }
 
 /**
+ * Output a comma-separated list of user_ids for a given user's followers.
+ *
+ * @param array $args See bp_get_follower_ids().
+ */
+function bp_follower_ids( $args = '' ) {
+	echo bp_get_follower_ids( $args );
+}
+	/**
+	 * Returns a comma separated list of user_ids for a given user's followers.
+	 *
+	 * On failure, returns an integer of zero. Needed when used in a members loop to prevent SQL errors.
+	 *
+	 * @param array $args {
+	 *     Array of arguments.
+	 *     @type int $user_id The user ID you want to check for followers.
+	 *     @type string $follow_type The follow type
+	 * }
+	 * @return string|int Comma-seperated string of user IDs on success. Integer zero on failure.
+	 */
+	function bp_get_follower_ids( $args = '' ) {
+
+		$r = wp_parse_args( $args, array(
+			'user_id' => bp_displayed_user_id()
+		) );
+
+		$ids = implode( ',', (array) bp_follow_get_followers( array( 'user_id' => $r['user_id'] ) ) );
+
+		$ids = empty( $ids ) ? 0 : $ids;
+
+		return apply_filters( 'bp_get_follower_ids', $ids, $r['user_id'] );
+	}
+
+/**
+ * Output a comma-separated list of user_ids for a given user's following.
+ *
+ * @param array $args See bp_get_following_ids().
+ */
+function bp_following_ids( $args = '' ) {
+	echo bp_get_following_ids( $args );
+}
+	/**
+	 * Returns a comma separated list of IDs for a given user's following.
+	 *
+	 * On failure, returns integer zero. Needed when used in a members loop to prevent SQL errors.
+	 *
+	 * @param array $args {
+	 *     Array of arguments.
+	 *     @type int $user_id The user ID to fetch following user IDs for.
+	 *     @type string $follow_type The follow type
+	 * }
+	 * @return string|int Comma-seperated string of user IDs on success. Integer zero on failure.
+	 */
+	function bp_get_following_ids( $args = '' ) {
+
+		$r = wp_parse_args( $args, array(
+			'user_id'     => bp_displayed_user_id(),
+			'follow_type' => '',
+		) );
+
+		$ids = implode( ',', (array) bp_follow_get_following( array(
+			'user_id'     => $r['user_id'],
+			'follow_type' => $r['follow_type'],
+		) ) );
+
+		$ids = empty( $ids ) ? 0 : $ids;
+
+		return apply_filters( 'bp_get_following_ids', $ids, $r['user_id'], $r );
+	}
+
+/**
  * Get the total followers and total following counts for a user.
  *
  * @since 1.0.0
