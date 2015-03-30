8a23b4f4d6f84d657b6ff1d4444079cd6670d048
r-a-y <r-a-y@gmx.com>
2015-03-30 14:23:51 -0700

    Boo Ray.

--- _inc/users/hooks.php
+++ _inc/users/hooks.php
@@ -962,7 +962,7 @@ function bp_follow_prime_mentions_results() {
 		return;
 	}
 
-	$following = bp_follow_get_following( array( 'user_id' => bp_loggedin_user_id() );
+	$following = bp_follow_get_following( array( 'user_id' => bp_loggedin_user_id() ) );
 	if ( empty( $following ) ) {
 		return;
 	}
