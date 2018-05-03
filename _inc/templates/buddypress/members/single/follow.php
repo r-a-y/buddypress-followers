ed47e20402ee459b90aeaf3dd996dec4daf4f758
r-a-y <r-a-y@gmx.com>
2018-05-03 12:43:07 -0700

    Nouveau: Alter 'members/single/follow' template to use feedback function.
    What this means is we don't have to query for the members loop when a user's
    "Following" or "Followers" page is loaded.  The members loop is later added
    via AJAX by Nouveau.
    
    See #97.

--- _inc/templates/buddypress/members/single/follow.php
+++ _inc/templates/buddypress/members/single/follow.php
@@ -3,7 +3,15 @@
 
 			<?php // this is important! do not remove the classes in this DIV as AJAX relies on it! ?>
 			<div id="members-dir-list" class="dir-list members follow <?php echo bp_current_action(); ?>" data-bp-list="members">
-				<?php bp_get_template_part( 'members/members-loop' ) ?>
+				<?php if ( function_exists( 'bp_nouveau' ) ) : ?>
+
+					<div id="bp-ajax-loader"><?php bp_nouveau_user_feedback( 'generic-loading' ); ?></div>
+				
+				<?php else : ?>
+
+					<?php bp_get_template_part( 'members/members-loop' ) ?>
+
+				<?php endif; ?>
 			</div>
 
 			<?php do_action( 'bp_after_member_' . bp_current_action() . '_content' ); ?>
