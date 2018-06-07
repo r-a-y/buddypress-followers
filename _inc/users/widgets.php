29ce249a7b984f450c905ed0e6abc0c7bbade768
r-a-y <r-a-y@gmx.com>
2018-06-07 16:33:14 -0700

    PHP 7.2: Replace create_function() with anonymous function.
    create_function() is deprecated in PHP 7.2.

--- _inc/users/widgets.php
+++ _inc/users/widgets.php
@@ -110,4 +110,6 @@ class BP_Follow_Following_Widget extends WP_Widget {
 	<?php
 	}
 }
-add_action( 'widgets_init', create_function( '', 'return register_widget("BP_Follow_Following_Widget");' ) );
+add_action( 'widgets_init', function() {
+	register_widget( 'BP_Follow_Following_Widget' );
+} );
