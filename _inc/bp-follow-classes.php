r-a-y/buddypress-followers

commit d7554bef9f8b9ea1f3748481af23a1b453bdcc95

WPCS: Fixing several WPCS fixes

_inc/bp-follow-classes.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * BP Follow Classes
+ * BP Follow Class
  *
  * @package BP-Follow
- * @subpackage Classes
+ * @subpackage Class
  */
 
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
 /**
  * BuddyPress Follow class.
@@ -62,9 +62,9 @@ class BP_Follow {
 	/**
 	 * Constructor.
 	 *
-	 * @param int $leader_id The ID of the item wewant to follow.
-	 * @param int $follower_id The ID initiating the follow request.
-	 * @param string $follow_type The type of follow connection.
+	 * @param int    $leader_id    The ID of the item wewant to follow.
+	 * @param int    $follower_id  The ID initiating the follow request.
+	 * @param string $follow_type  The type of follow connection.
 	 */
 	public function __construct( $leader_id = 0, $follower_id = 0, $follow_type = '' ) {
 		if ( ! empty( $leader_id ) && ! empty( $follower_id ) ) {
@@ -86,42 +86,42 @@ class BP_Follow {
 	protected function populate() {
 		global $wpdb;
 
-		// we always require a leader ID
+		// we always require a leader ID.
 		if ( empty( $this->leader_id ) ) {
 			return;
 		}
 
-		// check cache first
+		// check cache first.
 		$key = "{$this->leader_id}:{$this->follower_id}:{$this->follow_type}";
 		$data = wp_cache_get( $key, 'bp_follow_data' );
 
-		// Run query if no cache
+		// Run query if no cache.
 		if ( false === $data ) {
-			// SQL statement
-			$sql =  self::get_select_sql( 'id, date_recorded' );
+			// SQL statement.
+			$sql = self::get_select_sql( 'id, date_recorded' );
 			$sql .= self::get_where_sql( array(
 				'leader_id'   => $this->leader_id,
 				'follower_id' => $this->follower_id,
 				'follow_type' => $this->follow_type,
 			) );
 
-			// Run the query
+			// Run the query.
 			$data = $wpdb->get_results( $sql );
 
-			// Got a match; grab the results
+			// Got a match; grab the results.
 			if ( ! empty( $data ) ) {
 				$data = $data[0];
 
-			// No match; set cache to zero to prevent further hits to database
+			// No match. Set cache to zero to prevent further hits to database.
 			} else {
 				$data = 0;
 			}
 
-			// Set the cache
+			// Set the cache.
 			wp_cache_set( $key, $data, 'bp_follow_data' );
 		}
 
-		// Populate some other properties
+		// Populate some other properties.
 		if ( ! empty( $data ) ) {
 			$this->id = $data->id;
 			$this->date_recorded = $data->date_recorded;
@@ -137,7 +137,7 @@ class BP_Follow {
 		global $wpdb, $bp;
 
 		// do not use these filters
-		// use the 'bp_follow_before_save' hook instead
+		// use the 'bp_follow_before_save' hook instead.
 		$this->leader_id   = apply_filters( 'bp_follow_leader_id_before_save',   $this->leader_id,   $this->id );
 		$this->follower_id = apply_filters( 'bp_follow_follower_id_before_save', $this->follower_id, $this->id );
 
@@ -145,43 +145,43 @@ class BP_Follow {
 
 		// leader ID is required
 		// this allows plugins to bail out of saving a follow relationship
-		// use hooks above to redeclare 'leader_id' so it is empty if you need to bail
+		// use hooks above to redeclare 'leader_id' so it is empty if you need to bail.
 		if ( empty( $this->leader_id ) ) {
 			return false;
 		}
 
-		// make sure a date is added for those directly using the save() method
+		// make sure a date is added for those directly using the save() method.
 		if ( empty( $this->date_recorded ) ) {
 			$this->date_recorded = bp_core_current_time();
 		}
 
-		// update existing entry
+		// update existing entry.
 		if ( $this->id ) {
 			$result = $wpdb->query( $wpdb->prepare(
-				"UPDATE {$bp->follow->table_name} SET leader_id = %d, follower_id = %d, follow_type = %s, date_recorded = %s WHERE id = %d",
-					$this->leader_id,
-					$this->follower_id,
-					$this->follow_type,
-					$this->date_recorded,
-					$this->id
+				$this->leader_id,
+				$this->follower_id,
+				$this->follow_type,
+				$this->date_recorded,
+				$this->id
 			) );
 
 		// add new entry
 		} else {
 			$result = $wpdb->query( $wpdb->prepare(
 				"INSERT INTO {$bp->follow->table_name} ( leader_id, follower_id, follow_type, date_recorded ) VALUES ( %d, %d, %s, %s )",
-					$this->leader_id,
-					$this->follower_id,
-					$this->follow_type,
-					$this->date_recorded
+				$this->leader_id,
+				$this->follower_id,
+				$this->follow_type,
+				$this->date_recorded
 			) );
 			$this->id = $wpdb->insert_id;
 		}
 
-		// Save cache
-		$data = new stdClass;
+		// Save cache.
+		$data = new stdClass();
 		$data->id = $this->id;
 		$data->date_recorded = $this->date_recorded;
+
 		wp_cache_set( "{$this->leader_id}:{$this->follower_id}:{$this->follow_type}", $data, 'bp_follow_data' );
 
 		do_action_ref_array( 'bp_follow_after_save', array( &$this ) );
@@ -197,13 +197,13 @@ class BP_Follow {
 	public function delete() {
 		global $wpdb, $bp;
 
-		// SQL statement
+		// SQL statement.
 		$sql  = "DELETE FROM {$bp->follow->table_name} ";
 		$sql .= self::get_where_sql( array(
 			'id' => $this->id,
 		) );
 
-		// Delete cache
+		// Delete cache.
 		wp_cache_delete( "{$this->leader_id}:{$this->follower_id}:{$this->follow_type}", 'bp_follow_data' );
 
 		return $wpdb->query( $sql );
@@ -216,23 +216,21 @@ class BP_Follow {
 	 *
 	 * @since 1.3.0
 	 *
-	 * @param string $column
+	 * @param string $column Column.
 	 * @return string
 	 */
 	protected static function get_select_sql( $column = '' ) {
-		global $bp;
-
-		return sprintf( "SELECT %s FROM %s ", esc_sql( $column ), esc_sql( $bp->follow->table_name ) );
+		return sprintf( 'SELECT %s FROM %s ', esc_sql( $column ), esc_sql( buddypress()->follow->table_name ) );
 	}
 
 	/**
 	 * Generate the WHERE SQL statement used to query follow relationships.
 	 *
-	 * @since 1.3.0
-	 *
 	 * @todo Add support for date ranges with 'date_recorded' column
 	 *
-	 * @param array $params
+	 * @since 1.3.0
+	 *
+	 * @param array $params Where params.
 	 * @return string
 	 */
 	protected static function get_where_sql( $params = array() ) {
@@ -256,11 +254,10 @@ class BP_Follow {
 		}
 
 		if ( isset( $params['follow_type'] ) ) {
-			$where_conditions['follow_type'] = $wpdb->prepare( "follow_type = %s", $params['follow_type'] );
+			$where_conditions['follow_type'] = $wpdb->prepare( 'follow_type = %s', $params['follow_type'] );
 		}
 
 		return 'WHERE ' . join( ' AND ', $where_conditions );
-
 	}
 
 	/**
@@ -281,28 +278,28 @@ class BP_Follow {
 			'order'   => 'DESC',
 		) );
 
-		// sanitize 'orderby' DB oclumn lookup
+		// sanitize 'orderby' DB oclumn lookup.
 		switch ( $r['orderby'] ) {
-			// columns available for lookup
-			case 'id' :
-			case 'leader_id' :
-			case 'follower_id' :
-			case 'follow_type' :
-			case 'date_recorded' :
+			// columns available for lookup.
+			case 'id':
+			case 'leader_id':
+			case 'follower_id':
+			case 'follow_type':
+			case 'date_recorded':
 				break;
 
-			// fallback to 'id' column on anything else
-			default :
+			// fallback to 'id' column on anything else.
+			default:
 				$r['orderby'] = 'id';
 				break;
 		}
 
-		// only allow ASC or DESC for order
+		// only allow ASC or DESC for order.
 		if ( 'ASC' !== $r['order'] || 'DESC' !== $r['order'] ) {
 			$r['order'] = 'DESC';
 		}
 
-		return sprintf( " ORDER BY %s %s", $r['orderby'], $r['order'] );
+		return sprintf( ' ORDER BY %s %s', $r['orderby'], $r['order'] );
 	}
 
 	/**
@@ -310,9 +307,9 @@ class BP_Follow {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param int $leader_id The leader ID.
+	 * @param int    $leader_id The leader ID.
 	 * @param string $follow_type The follow type.  Leave blank to query users.
-	 * @param array $query_args {
+	 * @param array  $query_args {
 	 *     Various query arguments
 	 *     @type array $date_query See {@link WP_Date_Query}.
 	 *     @type string $orderby The DB column to order results by. Default: 'id'.
@@ -323,14 +320,14 @@ class BP_Follow {
 	public static function get_followers( $leader_id = 0, $follow_type = '', $query_args = array() ) {
 		global $wpdb;
 
-		// SQL statement
+		// SQL statement.
 		$sql  = self::get_select_sql( 'follower_id' );
 		$sql .= self::get_where_sql( array(
 			'leader_id'   => $leader_id,
 			'follow_type' => $follow_type,
 		) );
 
-		// Setup date query
+		// Setup date query.
 		if ( ! empty( $query_args['date_query'] ) && class_exists( 'WP_Date_Query' ) ) {
 			add_filter( 'date_query_valid_columns', array( __CLASS__, 'register_date_column' ) );
 			$date_query = new WP_Date_Query( $query_args['date_query'], 'date_recorded' );
@@ -338,7 +335,7 @@ class BP_Follow {
 			remove_filter( 'date_query_valid_columns', array( __CLASS__, 'register_date_column' ) );
 		}
 
-		// Setup orderby query
+		// Setup orderby query.
 		$orderby = array();
 		if ( ! empty( $query_args['orderby'] ) ) {
 			$orderby = $query_args['orderby'];
@@ -348,7 +345,7 @@ class BP_Follow {
 		}
 		$sql .= self::get_orderby_sql( $orderby );
 
-		// do the query
+		// do the query.
 		return $wpdb->get_col( $sql );
 	}
 
@@ -370,14 +367,14 @@ class BP_Follow {
 	public static function get_following( $user_id = 0, $follow_type = '', $query_args = array() ) {
 		global $wpdb;
 
-		// SQL statement
+		// SQL statement.
 		$sql  = self::get_select_sql( 'leader_id' );
 		$sql .= self::get_where_sql( array(
 			'follower_id' => $user_id,
 			'follow_type' => $follow_type,
 		) );
 
-		// Setup date query
+		// Setup date query.
 		if ( ! empty( $query_args['date_query'] ) && class_exists( 'WP_Date_Query' ) ) {
 			add_filter( 'date_query_valid_columns', array( __CLASS__, 'register_date_column' ) );
 			$date_query = new WP_Date_Query( $query_args['date_query'], 'date_recorded' );
@@ -385,7 +382,7 @@ class BP_Follow {
 			remove_filter( 'date_query_valid_columns', array( __CLASS__, 'register_date_column' ) );
 		}
 
-		// Setup orderby query
+		// Setup orderby query.
 		$orderby = array();
 		if ( ! empty( $query_args['orderby'] ) ) {
 			$orderby = $query_args['orderby'];
@@ -395,7 +392,7 @@ class BP_Follow {
 		}
 		$sql .= self::get_orderby_sql( $orderby );
 
-		// do the query
+		// do the query.
 		return $wpdb->get_col( $sql );
 	}
 
@@ -405,7 +402,7 @@ class BP_Follow {
 	 * @since 1.3.0
 	 *
 	 * @param int    $leader_id   The leader ID to grab the followers count for.
-	 * @param string $follow_type The follow type. Leave blank to query for users.  Default: ''
+	 * @param string $follow_type The follow type. Leave blank to query for users.
 	 * @return int
 	 */
 	public static function get_followers_count( $leader_id = 0, $follow_type = '' ) {
@@ -426,7 +423,7 @@ class BP_Follow {
 	 * @since 1.3.0
 	 *
 	 * @param int    $id          The object ID to grab the following count for.
-	 * @param string $follow_type The follow type. Leave blank to query for users. Default: ''
+	 * @param string $follow_type The follow type. Leave blank to query for users.
 	 * @return int
 	 */
 	public static function get_following_count( $id = 0, $follow_type = '' ) {
@@ -454,7 +451,10 @@ class BP_Follow {
 		$following = self::get_following_count( $id, $follow_type );
 		$followers = self::get_followers_count( $id, $follow_type );
 
-		return array( 'followers' => $followers, 'following' => $following );
+		return array(
+			'followers' => $followers,
+			'following' => $following,
+		);
 	}
 
 	/**
@@ -462,8 +462,8 @@ class BP_Follow {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @param array $leader_ids The user IDs to check the follow status for.
-	 * @param int $user_id The user ID to check against the list of leader IDs.
+	 * @param array  $leader_ids The user IDs to check the follow status for.
+	 * @param int    $user_id The user ID to check against the list of leader IDs.
 	 * @param string $follow_type The type of follow connection.
 	 * @return array
 	 */
@@ -478,7 +478,7 @@ class BP_Follow {
 			return false;
 		}
 
-		// SQL statement
+		// SQL statement.
 		$sql  = self::get_select_sql( 'leader_id, id' );
 		$sql .= self::get_where_sql( array(
 			'follower_id' => $user_id,
@@ -494,7 +494,7 @@ class BP_Follow {
 	 *
 	 * @since 1.1.0
 	 *
-	 * @param int $user_id The user ID
+	 * @param int $user_id The user ID.
 	 */
 	public static function delete_all_for_user( $user_id = 0 ) {
 		global $bp, $wpdb;
@@ -507,11 +507,12 @@ class BP_Follow {
 	 *
 	 * @since 1.3.0
 	 *
-	 * @param array $retval Current DB columns
+	 * @param array $retval Current DB columns.
 	 * @return array
 	 */
 	public static function register_date_column( $retval ) {
 		$retval[] = 'date_recorded';
+
 		return $retval;
 	}
 }

_inc/bp-follow-functions.php
@@ -6,8 +6,8 @@
  * @subpackage Functions
  */
 
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Start following an item.
@@ -24,7 +24,6 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * @return bool
  */
 function bp_follow_start_following( $args = '' ) {
-	global $bp;
 
 	$r = wp_parse_args( $args, array(
 		'leader_id'     => bp_displayed_user_id(),
@@ -35,12 +34,12 @@ function bp_follow_start_following( $args = '' ) {
 
 	$follow = new BP_Follow( $r['leader_id'], $r['follower_id'], $r['follow_type'] );
 
-	// existing follow already exists
+	// existing follow already exists.
 	if ( ! empty( $follow->id ) ) {
 		return false;
 	}
 
-	// add other properties before save
+	// add other properties before save.
 	$follow->date_recorded = $r['date_recorded'];
 
 	// save!
@@ -149,14 +148,14 @@ function bp_follow_get_followers( $args = '' ) {
 	$retval   = array();
 	$do_query = true;
 
-	// Set up filter name
+	// Set up filter name.
 	if ( ! empty( $r['follow_type'] ) ) {
-		$filter = 'bp_follow_get_followers_' .  $r['object'];
+		$filter = 'bp_follow_get_followers_' . $r['object'];
 	} else {
 		$filter = 'bp_follow_get_followers';
 	}
 
-	// check for cache if 'query_args' is empty
+	// check for cache if 'query_args' is empty.
 	if ( empty( $r['query_args'] ) ) {
 		$retval = wp_cache_get( $r['object_id'], "bp_follow_{$r['object']}_followers_query" );
 
@@ -165,15 +164,15 @@ function bp_follow_get_followers( $args = '' ) {
 		}
 	}
 
-	// query if necessary
+	// query if necessary.
 	if ( true === $do_query ) {
 		$retval = BP_Follow::get_followers( $r['object_id'], $r['follow_type'], $r['query_args'] );
 
-		// cache if no extra query args - we only cache default args for now
+		// cache if no extra query args - we only cache default args for now.
 		if ( empty( $r['query_args'] ) ) {
 			wp_cache_set( $r['object_id'], $retval, "bp_follow_{$r['object']}_followers_query" );
 
-			// cache count while we're at it
+			// cache count while we're at it.
 			wp_cache_set( $r['object_id'], $GLOBALS['wpdb']->num_rows, "bp_follow_{$r['object']}_followers_count" );
 		}
 	}
@@ -215,14 +214,14 @@ function bp_follow_get_following( $args = '' ) {
 	$retval   = array();
 	$do_query = true;
 
-	// setup some variables based on the follow type
+	// setup some variables based on the follow type.
 	if ( ! empty( $r['follow_type'] ) ) {
-		$filter = 'bp_follow_get_following_' .  $r['object'];
+		$filter = 'bp_follow_get_following_' . $r['object'];
 	} else {
 		$filter = 'bp_follow_get_following';
 	}
 
-	// check for cache if 'query_args' is empty
+	// check for cache if 'query_args' is empty.
 	if ( empty( $r['query_args'] ) ) {
 		$retval = wp_cache_get( $r['object_id'], "bp_follow_{$r['object']}_following_query" );
 
@@ -231,15 +230,15 @@ function bp_follow_get_following( $args = '' ) {
 		}
 	}
 
-	// query if necessary
+	// query if necessary.
 	if ( true === $do_query ) {
 		$retval = BP_Follow::get_following( $r['object_id'], $r['follow_type'], $r['query_args'] );
 
-		// cache if no extra query args - we only cache default args for now
+		// cache if no extra query args - we only cache default args for now.
 		if ( empty( $r['query_args'] ) ) {
 			wp_cache_set( $r['object_id'], $retval, "bp_follow_{$r['object']}_following_query" );
 
-			// cache count while we're at it
+			// cache count while we're at it.
 			wp_cache_set( $r['object_id'], $GLOBALS['wpdb']->num_rows, "bp_follow_{$r['object']}_following_count" );
 		}
 	}
@@ -281,10 +280,12 @@ function bp_follower_ids( $args = '' ) {
 	function bp_get_follower_ids( $args = '' ) {
 
 		$r = wp_parse_args( $args, array(
-			'user_id' => bp_displayed_user_id()
+			'user_id' => bp_displayed_user_id(),
 		) );
 
-		$ids = implode( ',', (array) bp_follow_get_followers( array( 'user_id' => $r['user_id'] ) ) );
+		$ids = implode( ',', (array) bp_follow_get_followers( array(
+			'user_id' => $r['user_id'],
+		) ) );
 
 		$ids = empty( $ids ) ? 0 : $ids;
 
@@ -358,7 +359,7 @@ function bp_follow_total_follow_counts( $args = '' ) {
 
 	$retval['following'] = bp_follow_get_the_following_count( array(
 		'user_id'     => $r['user_id'],
-		'follow_type' => $r['follow_type']
+		'follow_type' => $r['follow_type'],
 	) );
 
 	/**
@@ -371,7 +372,7 @@ function bp_follow_total_follow_counts( $args = '' ) {
 	} else {
 		$retval['followers'] = bp_follow_get_the_followers_count( array(
 			'user_id'     => $r['user_id'],
-			'follow_type' => $r['follow_type']
+			'follow_type' => $r['follow_type'],
 		) );
 	}
 
@@ -408,16 +409,16 @@ function bp_follow_total_follow_counts( $args = '' ) {
  *
  * @since 1.3.0
  *
- * @param  array $args See bp_follow_get_common_args()
+ * @param  array $args See bp_follow_get_common_args().
  * @return int
  */
 function bp_follow_get_the_following_count( $args = array() ) {
 	$r = bp_follow_get_common_args( $args );
 
-	// fetch cache
+	// fetch cache.
 	$retval = wp_cache_get( $r['object_id'], "bp_follow_{$r['object']}_following_count" );
 
-	// query if necessary
+	// query if necessary.
 	if ( false === $retval ) {
 		$retval = BP_Follow::get_following_count( $r['object_id'], $r['follow_type'] );
 		wp_cache_set( $r['object_id'], $retval, "bp_follow_{$r['object']}_following_count" );
@@ -443,16 +444,16 @@ function bp_follow_get_the_following_count( $args = array() ) {
  *
  * @since 1.3.0
  *
- * @param  array $args See bp_follow_get_common_args()
+ * @param  array $args See bp_follow_get_common_args().
  * @return int
  */
 function bp_follow_get_the_followers_count( $args = array() ) {
 	$r = bp_follow_get_common_args( $args );
 
-	// fetch cache
+	// fetch cache.
 	$retval = wp_cache_get( $r['object_id'], "bp_follow_{$r['object']}_followers_count" );
 
-	// query if necessary
+	// query if necessary.
 	if ( false === $retval ) {
 		$retval = BP_Follow::get_followers_count( $r['object_id'], $r['follow_type'] );
 		wp_cache_set( $r['object_id'], $retval, "bp_follow_{$r['object']}_followers_count" );
@@ -493,7 +494,7 @@ function bp_follow_get_common_args( $args = array() ) {
 		'user_id'     => bp_loggedin_user_id(),
 		'follow_type' => '',
 		'object_id'   => '',
-		'query_args'  => array()
+		'query_args'  => array(),
 	) );
 
 	// Set up our object. $object is used for cache keys and filter names.
@@ -505,7 +506,7 @@ function bp_follow_get_common_args( $args = array() ) {
 			$object = $r['follow_type'];
 		}
 
-	// Defaults to 'user'
+	// Defaults to 'user'.
 	} else {
 		$object = 'user';
 	}
@@ -520,7 +521,7 @@ function bp_follow_get_common_args( $args = array() ) {
 		'object'      => $object,
 		'object_id'   => $object_id,
 		'follow_type' => $r['follow_type'],
-		'query_args'  => $r['query_args']
+		'query_args'  => $r['query_args'],
 	);
 }
 
@@ -553,14 +554,13 @@ function bp_follow_is_doing_ajax() {
  */
 function bp_follow_notification_settings_content() {
 ?>
-
 	<table class="notification-settings" id="follow-notification-settings">
 		<thead>
 			<tr>
 				<th class="icon"></th>
-				<th class="title"><?php _e( 'Follow', 'buddypress-followers' ) ?></th>
-				<th class="yes"><?php _e( 'Yes', 'buddypress-followers' ) ?></th>
-				<th class="no"><?php _e( 'No', 'buddypress-followers' )?></th>
+				<th class="title"><?php esc_html_e( 'Follow', 'buddypress-followers' ); ?></th>
+				<th class="yes"><?php esc_html_e( 'Yes', 'buddypress-followers' ); ?></th>
+				<th class="no"><?php esc_html_e( 'No', 'buddypress-followers' ); ?></th>
 			</tr>
 		</thead>
 
@@ -577,17 +577,17 @@ function bp_follow_notification_settings_content() {
  * @global $bp The global BuddyPress settings variable created in bp_core_setup_globals()
  */
 function bp_follow_format_notifications( $action, $item_id, $secondary_item_id, $total_items, $format = 'string' ) {
-	global $bp;
 
 	do_action( 'bp_follow_format_notifications', $action, $item_id, $secondary_item_id, $total_items, $format );
 
 	switch ( $action ) {
 		case 'new_follow':
-			$link = $text = false;
+			$text = false;
+			$link = $text;
 
-			if ( 1 == $total_items ) {
+			if ( 1 === $total_items ) {
 				$text = sprintf( __( '%s is now following you', 'buddypress-followers' ), bp_core_get_user_displayname( $item_id ) );
-				$link = bp_core_get_user_domain( $item_id  ) . '?bpf_read';
+				$link = bp_core_get_user_domain( $item_id ) . '?bpf_read';
 
 			} else {
 				$text = sprintf( __( '%d more users are now following you', 'buddypress-followers' ), $total_items );
@@ -595,34 +595,34 @@ function bp_follow_format_notifications( $action, $item_id, $secondary_item_id, 
 				if ( bp_is_active( 'notifications' ) ) {
 					$link = bp_get_notifications_permalink();
 
-					// filter notifications by 'new_follow' action
+					// filter notifications by 'new_follow' action.
 					if ( version_compare( BP_VERSION, '2.0.9' ) >= 0 ) {
 						$link .= '?action=' . $action;
 					}
 				} else {
-					$link = bp_loggedin_user_domain() . $bp->follow->followers->slug . '/?new';
+					$link = bp_loggedin_user_domain() . buddypress()->follow->followers->slug . '/?new';
 				}
 			}
 
-		break;
+			break;
 
-		default :
+		default:
 			$link = apply_filters( 'bp_follow_extend_notification_link', false, $action, $item_id, $secondary_item_id, $total_items );
 			$text = apply_filters( 'bp_follow_extend_notification_text', false, $action, $item_id, $secondary_item_id, $total_items );
-		break;
+			break;
 	}
 
 	if ( ! $link || ! $text ) {
 		return false;
 	}
 
-	if ( 'string' == $format ) {
+	if ( 'string' === $format ) {
 		return apply_filters( 'bp_follow_new_followers_notification', '<a href="' . $link . '">' . $text . '</a>', $total_items, $link, $text, $item_id, $secondary_item_id );
 
 	} else {
 		$array = array(
 			'text' => $text,
-			'link' => $link
+			'link' => $link,
 		);
 
 		return apply_filters( 'bp_follow_new_followers_return_notification', $array, $item_id, $secondary_item_id, $total_items );

_inc/bp-follow-updater.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * BP Follow Updater
+ *
+ * @package BP-Follow
+ * @subpackage Updater
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Updater class.
@@ -6,6 +15,7 @@
  * @since 1.3.0
  */
 class BP_Follow_Updater {
+
 	/**
 	 * Constructor.
 	 *
@@ -40,12 +50,12 @@ class BP_Follow_Updater {
 		$installed_date = (int) self::get_installed_revision_date();
 
 		// May 5, 2014    - added 'follow_type' DB column
-		// August 7, 2014 - added 'date_recorded' DB column
+		// August 7, 2014 - added 'date_recorded' DB column.
 		if ( $installed_date < 1399352400 || $installed_date < 1407448800 ) {
 			$this->install();
 		}
 
-		// bump revision date in DB
+		// bump revision date in DB.
 		self::bump_revision_date();
 	}
 
@@ -55,11 +65,14 @@ class BP_Follow_Updater {
 	 * Installs the BP Follow DB table.
 	 */
 	protected function install() {
-		global $bp, $wpdb;
+		global $wpdb;
 
-		$charset_collate = ! empty( $wpdb->charset ) ? "DEFAULT CHARACTER SET $wpdb->charset" : '';
+		$charset_collate = ! empty( $wpdb->charset )
+			? "DEFAULT CHARACTER SET $wpdb->charset"
+			: '';
 
-		if ( ! $table_prefix = $bp->table_prefix ) {
+		$table_prefix = buddypress()->table_prefix;
+		if ( ! $table_prefix ) {
 			$table_prefix = apply_filters( 'bp_core_get_table_prefix', $wpdb->base_prefix );
 		}
 
@@ -90,9 +103,7 @@ class BP_Follow_Updater {
 			return false;
 		}
 
-		global $bp;
-
-		return $bp->follow->revision_date;
+		return buddypress()->follow->revision_date;
 	}
 
 	/**
@@ -125,9 +136,7 @@ class BP_Follow_Updater {
 	 * @return bool
 	 */
 	public static function is_loaded() {
-		global $bp;
-
-		if ( empty( $bp->follow ) ) {
+		if ( empty( buddypress()->follow ) ) {
 			return false;
 		}
 

commit 6baa72b966fc779c88872684c8fbfc2b9e82da23

Fix updating an existing follow item.
Broke in commit d7554be.

_inc/bp-follow-classes.php
@@ -158,6 +158,7 @@ class BP_Follow {
 		// update existing entry.
 		if ( $this->id ) {
 			$result = $wpdb->query( $wpdb->prepare(
+				"UPDATE {$bp->follow->table_name} SET leader_id = %d, follower_id = %d, follow_type = %s, date_recorded = %s WHERE id = %d",
 				$this->leader_id,
 				$this->follower_id,
 				$this->follow_type,